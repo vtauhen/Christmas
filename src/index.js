240a5f02551b82d346214d7831d0909f6eff169f
Henadzi Baranau <Henadzi.Baranau@bglgroup.co.uk>
2018-12-20 12:51:49 +0300

    Add maze and oleg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import $ from 'jquery';
+import Maze from './maze';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let currentPosition = { x: 0, y: 0 };
+let allowedKeys = [37, 38, 39, 40];
+let disp = new Maze(20, 20);
+disp[19][19][1] = 1;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+drawMaze();
+
+$(document).keydown(function (e) {
+    if (allowedKeys.indexOf(e.which) == -1) {
+        return;
+    }
+
+    let { x, y } = currentPosition;
+    switch (e.which) {
+        case 37: // left
+            if (disp[y][x][3] !== 0) {
+                x--;
+            }
+            break;
+        case 38: // up
+            if (disp[y][x][0] !== 0) {
+                y--;
+            }
+            break;
+        case 39: // right
+            if (disp[y][x][1] !== 0) {
+                x++;
+            }
+            break;
+        case 40: // down
+            if (disp[y][x][2] !== 0) {
+                y++;
+            }
+            break;
+        default: return; // exit this handler for other keys
+    }
+    currentPosition = { x: x, y: y };
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+    drawMaze();
+});
+
+
+function drawMaze() {
+    $('#maze > tbody').empty();
+    for (var i = 0; i < disp.length; i++) {
+        $('#maze > tbody').append("<tr>");
+        for (var j = 0; j < disp[i].length; j++) {
+            var selector = i + "-" + j;
+            $('#maze > tbody').append("<td id='" + selector + "'>&nbsp;</td>");
+            if (disp[i][j][0] == 0) { $('#' + selector).css('border-top', '2px solid black'); }
+            if (disp[i][j][1] == 0) { $('#' + selector).css('border-right', '2px solid black'); }
+            if (disp[i][j][2] == 0) { $('#' + selector).css('border-bottom', '2px solid black'); }
+            if (disp[i][j][3] == 0) { $('#' + selector).css('border-left', '2px solid black'); }
+        }
+        $('#maze > tbody').append("</tr>");
+    }
+    $('#' + currentPosition.y + '-' + currentPosition.x).append("<img id='oleg' src='./img/oleg.png' />");
+};
